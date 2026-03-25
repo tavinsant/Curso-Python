678bdcad3c05a47b639dd1c82bfbca4f97f50b83
tavinsant <otaviosantosdemoura2.1@gmail.com>
2026-03-25 20:01:13 -0300

    Add files via upload

---
+++ m.py
@@ -0,0 +1,9 @@
+
+print("Qual é o seu nome")
+nome=input()
+print("Qual é o eu sobrenome")
+sobrenome=input()
+print("quantos anos voce tem")
+anos=input()
+print("seu nome completo é")
+print(nome +sobrenome)
