f71cb38a862b1ee4abe6bed1b6c0b616d0f595b9
tavinsant <otaviosantosdemoura2.1@gmail.com>
2026-04-01 20:05:07 -0300

    exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+# Escreva  um algoritimo pra perguntar a média salarial da turma
+print("Base")
+base=int(input())
+print("Altura")
+altura=int(input())
+area=base*altura
+print("Sua área e de")
+print(area,"cm²")
