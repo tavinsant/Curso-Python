72382bbe09b972dc26fd92791d3d68cd40f5e49c
tavinsant <otaviosantosdemoura2.1@gmail.com>
2026-04-01 19:45:52 -0300

    Create exercico2.py

---
+++ exercico2.py
@@ -0,0 +1,13 @@
+# Escreva  um algoritimo pra perguntar a média salarial da turma
+print("Primeira nota")
+nota1=int(input())
+print("Segunda nota")
+nota2=int(input())
+print("Terceira nota")
+nota3=int(input())
+print("Quarta Nota")
+nota4=int(input())
+passo1=nota1+nota2+nota3+nota4
+passo2=passo1/4
+print("Media das notas")
+print(passo2)
